636329d17257316c5c2f2723e6c5a74b4f6c034b
valleaster <vallirieeasterjohnson@gmail.com>
2022-12-09 17:23:00 +0800

    Update code

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-// Typing
+// Typing delay
+var delayTime = 1000;
+
+if (window.innerWidth < 768) {
+    delayTime = 0;
+} else {
+    delayTime = 1000;
+}
+
 var typed = new Typed("#typed", {
     strings: ["Vallirie Easter.", "a Web Developer.", "a potato :)", "Vallirie Easter."],
-    startDelay: 1000,
+    startDelay: delayTime,
     typeSpeed: 80,
     backSpeed: 65,
     loop: false
@@ -16,7 +24,6 @@ var typed = new Typed("#typed", {
     var prevScroll = w.scrollY || doc.scrollTop;
     var curDirection = 0;
     var prevDirection = 0;
-
     var navbar = document.querySelector(".navbar");
     var toggled;
     var threshold = 200;
@@ -24,11 +31,9 @@ var typed = new Typed("#typed", {
     var checkScroll = function() {
         curScroll = w.scrollY || doc.scrollTop;
         if(curScroll > prevScroll) {
-            // scrolled down
             curDirection = 2;
         }
         else {
-            //scrolled up
             curDirection = 1;
         }
 
@@ -62,6 +67,34 @@ var typed = new Typed("#typed", {
 
 window.addEventListener('scroll', () => {
     document.querySelector(".navbar").classList.toggle("window-scroll", window.scrollY > 100);
+});
+
+// Mobile navbar (mostly)
+const hamburgerButton = document.getElementsByClassName("hamburger")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+const body = document.body;
+const navbarLinksItems = document.querySelectorAll('.navbar-links a');
+
+hamburgerButton.addEventListener('click', () => {
+    event.preventDefault();
+    body.classList.toggle('active');
+    navbarLinks.classList.toggle('active');
+});
+
+navbarLinksItems.forEach((item) => {
+    item.addEventListener('click', () => {
+        event.preventDefault();
+        const targetId = item.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        targetElement.scrollIntoView({ 
+            behavior: 'smooth', 
+            block: 'start', 
+            inline: 'nearest',
+            offset: 100,
+        });
+      navbarLinks.classList.toggle('active');
+      body.classList.toggle('active');
+    });
 });
 
 // Animation load on scroll
